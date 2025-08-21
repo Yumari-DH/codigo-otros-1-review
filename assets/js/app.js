8a8be17fd8b2ea113cc952addd8d82ef1e61927f
Yumari DH <yumari.diaz.herrera@gmail.com>
2025-08-21 11:18:40 -0600

    Se corrigió error en querySelector en js y se agregó la etiqueta que faltaba en el html, se corrigió error en fn asíncrona y definición de const data, que no estaba definida.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,15 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name');//se agrega el punto porque es una clase
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+//Se agrega palabra reservada async 
+async function displayUser(username) {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  //data no está definido, definimos data como la respuesta de fetch
+  const data = response.json();
   console.log(data);
   $n.textContent = '${data.name}';
   $b.textContent = '${data.blog}';
@@ -20,3 +23,6 @@ function handleError(err) {
 }
 
 displayUser('stolinski').catch(handleError);
+
+//Nota: Todavía me cuesta odentificar algunos errores, necesitaría mas tiempo para completarlo
+//voy a completar el ejercicio pero después de la hora de entrega, para continuar con los siguientes
